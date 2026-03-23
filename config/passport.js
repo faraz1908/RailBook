355049375b6c52e37ce211e0df3cf894e2892319
Faraz Ahmed <faraz1908khan@gmail.com>
2026-03-23 19:08:00 +0530

    fix: force update callback for production

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ module.exports = function(passport) {
   passport.use(new GoogleStrategy({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/auth/google/callback"
+      callbackURL: "https://railbook-3mys.onrender.com/auth/google/callback"
     },
     async (accessToken, refreshToken, profile, done) => {
       const newUser = {
